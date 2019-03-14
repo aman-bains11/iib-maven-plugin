a580cfc076d9aea9cb9eb1186553e2dd2fb778d7
AMAN BAINS <aman.bains@hbc.com>
2019-03-14 15:39:18 -0400

    Update DependencyPredicate.java

--- src/main/java/ch/sbb/maven/plugins/iib/utils/DependencyPredicate.java
+++ src/main/java/ch/sbb/maven/plugins/iib/utils/DependencyPredicate.java
@@ -7,12 +7,10 @@ import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.collections.Predicate;
 
 /**
+ *
+ *
+ *
  * 
- * 
- * 
- * @author u219237 (Pascal Moser)
- * @version $Id: $
- * @since 3.1, 2015
  */
 public class DependencyPredicate implements Predicate {
 
